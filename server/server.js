167f3ba7756aad867c53e7a7446f4e58f721a613
Noor Mudassar <noormudassar97@gmail.com>
2023-07-26 14:48:07 -0400

    changing port

--- server/server.js
+++ server/server.js
@@ -8,4 +8,4 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.listen(3000, () => console.log(`server running on 3000`));
+app.listen(5000, () => console.log(`server running on 5000`));
