39eb17559009099150dc09d177d7128c8900286d
Noor Mudassar <noormudassar97@gmail.com>
2023-07-26 12:56:54 -0400

    server is set up

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`public`));
 
-app.listen(4000, () => console.log(`server running on 4000`));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.listen(3000, () => console.log(`server running on 3000`));
